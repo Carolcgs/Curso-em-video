0a20c6fabbd101cf5bc683e27226724e7ac0689d
Caroline Gonçalves <carol.cgsma@gmail.com>
2022-06-12 11:53:42 -0300

    .

---
+++ JavaScript/aula11/exe008.js
@@ -0,0 +1,7 @@
+var val = 12
+console.log(`A velocidade do seu carro é ${val}Km/h`)
+if(val >60){
+    console.log(`Você ultrapassou a velocidade.MULTADO`)
+}
+
+console.log('Dirija sempre usando cinto de segurança')
